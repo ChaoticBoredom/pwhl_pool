a03fd22171a7e39958bff0718bf2be673d25d88d
Susan Wright <wright1@ualberta.ca>
2026-04-23 20:40:35 -0600

    Add fallback to ensure rails still serves up the React page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::API
+  include ActionView::Layouts
+  include ActionController::Rendering
   include ActionController::HttpAuthentication::Token::ControllerMethods
   include Authentication
 
   def current_user
     Current.user ||= Current.session&.user
+  end
+
+  def frontend
+    render file: Rails.root.join('public', 'index.html'), layout: false
   end
 
   # Only do this if you are strictly an API and understand the risks
