21c8101a9ffa8db734b078c5f0890ec0da0bd540
Susan Wright <wright1@ualberta.ca>
2026-04-23 22:23:48 -0600

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "application#frontend"
   get "*path", to: "application#frontend", constraints: ->(request) { !request.xhr? && request.format.html? }
 end
