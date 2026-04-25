da103d4beb357a9a9e9270ee31159df41e57e171
Susan Wright <wright1@ualberta.ca>
2026-04-24 18:36:17 -0600

    Scope the new position list to the pool

--- app/models/pool/box.rb
+++ app/models/pool/box.rb
@@ -3,6 +3,7 @@ class Pool::Box < ApplicationRecord
   validates :name, uniqueness: { scope: :pool_id, message: "all pool box names must be unique" }
 
   belongs_to :pool
+  positioned on: :pool
 
   def players
     @players ||= League::Player.where(id: league_player_ids)
