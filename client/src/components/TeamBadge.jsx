214828c1cead2811badf623114617b9f36e5e7b0
Susan Wright <wright1@ualberta.ca>
2026-04-29 03:26:46 -0600

    Remove extra commit

--- client/src/components/TeamBadge.jsx
+++ client/src/components/TeamBadge.jsx
@@ -4,8 +4,6 @@ import { PWHL_TEAMS } from '../constants/teams';
 const TeamBadge = ({ short_code }) => {
   const teamInfo = PWHL_TEAMS[short_code] || PWHL_TEAMS['default'];
 
-  console.log(short_code, teamInfo)
-
   return (
     <span className="team-badge-small" style={{ color: teamInfo.color }}>
       {short_code}
