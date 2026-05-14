d3d40c4da00b92960aedf34d144c2637cd549cf8
Susan Wright <wright1@ualberta.ca>
2026-05-14 02:06:58 -0600

    Set the 'start_time' and 'season_id' on the pwhl_player_stat when creating records

--- app/models/concerns/pwhl_player_stat.rb
+++ app/models/concerns/pwhl_player_stat.rb
@@ -2,7 +2,9 @@ module PwhlPlayerStat
   extend ActiveSupport::Concern
 
   included do
-    validates :goals, :assists, :penalty_minutes, :time_on_ice, presence: true
+    before_validation :add_game_attributes, if: -> { league_game.present? }, on: :create
+
+    validates :goals, :assists, :penalty_minutes, :time_on_ice, :start_time, :season_id, presence: true
 
     validates :league_player_id, uniqueness: { scope: :league_game_id, message: "should have one game stat per player" }
 
@@ -10,13 +12,16 @@ module PwhlPlayerStat
     belongs_to :league_game, class_name: "League::Game"
     belongs_to :league_player, class_name: "League::Player"
 
-    delegate :start_time, :season_id, to: :league_game
+    scope :for_date_range, ->(date_range) { where(start_time: date_range) }
+    scope :for_season, ->(season_id) { where(season_id: season_id) }
 
-    scope :for_date_range, ->(date_range) { joins(:league_game).where(league_game: { start_time: date_range }) }
-    scope :for_season, ->(season_id) { joins(:league_game).where(league_game: { season_id: season_id }) }
+    def add_game_attributes
+      self.start_time = league_game.start_time
+      self.season_id = league_game.season_id
+    end
 
     def self.for_date(date)
-      joins(:league_game).where(league_game: { start_time: date.all_day }).first
+      where(start_time: date.all_day).first
     end
   end
 end
