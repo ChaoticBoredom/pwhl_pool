3f7e9562a4fe2938ac5c6177f3928823dfdcc536
Susan Wright <wright1@ualberta.ca>
2026-05-18 22:37:51 -0600

    Add db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_16_092712) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_19_040547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -72,6 +72,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_16_092712) do
   end
 
   create_table "pool_boxes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.boolean "active", default: true, null: false
     t.datetime "created_at", null: false
     t.uuid "league_player_ids", default: [], null: false, array: true
     t.string "name", null: false
