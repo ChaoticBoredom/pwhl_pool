3a3d93207d2f4c7cf96fb8de954b675b36f582f3
Susan Wright <wright1@ualberta.ca>
2026-06-16 04:37:47 -0600

    Remove old 'update_team' endpoint (now handled via trade requests)

--- app/controllers/pool_teams_controller.rb
+++ app/controllers/pool_teams_controller.rb
@@ -38,43 +38,6 @@ class PoolTeamsController < ApplicationController
     end
   end
 
-  def update_roster
-    @pool_team = Pool::Team.find(params[:id])
-    return head :forbidden unless current_user == @pool_team.owner
-
-    @pool = @pool_team.pool
-    if @pool.trading_blocked?
-      render json: { error: "Trades are currently locked for this pool", reason: "trades_closed" }, status: :forbidden
-      return
-    end
-
-    box_by_player_id = @pool.active_box_by_player_id
-
-    original_team = @pool_team.current_team.pluck(:league_player_id)
-    new_team = params[:new_player_ids]
-
-    dropping = original_team - new_team
-    @pool_team.current_team.where(league_player_id: dropping).update(dropped_at: Time.current)
-
-    adding = new_team - original_team
-    adding.each do |pid|
-      @pool_team.pool_team_players.create(
-        league_player_id: pid,
-        added_at: Time.current,
-        pool_box: box_by_player_id[pid]
-      )
-    end
-
-    dropped_names = League::Player.where(id: dropping).pluck(:name)
-    added_names = League::Player.where(id: adding).pluck(:name)
-
-    render json: {
-      message: "Roster updated!",
-      added_players: added_names,
-      dropped_players: dropped_names,
-    }
-  end
-
   private
 
   def team_params
