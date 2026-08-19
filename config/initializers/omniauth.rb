e5af12361b068e6a454dde5e8e46c6db63a2829c
Susan Wright <wright1@ualberta.ca>
2026-08-19 17:17:45 -0600

    Clean up CSRF tokens again...

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ENV.fetch("GOOGLE_CLIENT_SECRET", nil)
 end
 
+OmniAuth.config.request_validation_phase = nil
+
 OmniAuth.config.on_failure = Proc.new do |env|
   if env["omniauth.error.type"] == :access_denied
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure
