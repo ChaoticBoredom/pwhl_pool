1542cdbe78159c8cc1e3f35c4e3e453e33dcf3d4
Susan Wright <wright1@ualberta.ca>
2026-08-19 18:01:29 -0600

    Clean up omniauth controller, add exception for brakeman (warning is incorrect)

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,22 +2,7 @@ class OmniauthCallbacksController < ApplicationController
   allow_unauthenticated_access only: %i[create failure]
 
   def create
-    auth = request.env["omniauth.auth"]
-    user = User.find_by(provider: auth.provider, uid: auth.uid) ||
-      User.find_by(email_address: auth.info.email)
-
-    if user
-      user.update!(provider: auth.provider, uid: auth.uid) if user.provider.blank?
-    else
-      user = User.create!(
-        email_address: auth.info.email,
-        name: auth.info.name,
-        password: SecureRandom.hex(32),
-        provider: auth.provider,
-        uid: auth.uid,
-      )
-    end
-
+    user = find_or_create_user(request.env["omniauth.auth"])
     session = start_new_session_for(user)
     code = Session.create_exchange_code_for(session)
 
@@ -30,7 +15,25 @@ class OmniauthCallbacksController < ApplicationController
 
   private
 
+  def find_or_create_user(auth)
+    user = User.find_by(provider: auth.provider, uid: auth.uid) ||
+      User.find_by(email_address: auth.info.email)
+
+    if user
+      user.update!(provider: auth.provider, uid: auth.uid) if user.provider.blank?
+      user
+    else
+      User.create!(
+        email_address: auth.info.email,
+        name: auth.info.name,
+        password: SecureRandom.hex(32),
+        provider: auth.provider,
+        uid: auth.uid,
+      )
+    end
+  end
+
   def frontend_url
-    Rails.application.credentials.dig(:frontend_url) || "http://localhost:5173"
+    ENV.fetch("FRONTEND_URL", "http://localhost:5173")
   end
 end
