40a5d35472034d56bcea086359ad58f6cd7985c6
Susan Wright <wright1@ualberta.ca>
2026-09-02 01:42:10 -0600

    Add validation of window date range, and specs

--- app/models/trade/window.rb
+++ app/models/trade/window.rb
@@ -1,7 +1,17 @@
 class Trade::Window < ApplicationRecord
   validates :open_window, presence: true
+  validate :open_window_start_before_end
 
   belongs_to :pool
 
   scope :current, -> { where("open_window @> ?::timestamptz", Time.current) }
+
+  private
+
+  def open_window_start_before_end
+    # Validations do not short circuit, need to ensure this is set or will throw unrelated errors
+    return if open_window.nil?
+
+    errors.add(:open_window, "start must be before end") if open_window.begin > open_window.end
+  end
 end
